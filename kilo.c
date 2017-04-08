6071a36a72c4266793d5f41bf0fb53ec37fec9b9
mysangle <mysangle@gmail.com>
2017-04-08 12:36:14 +0900

    step 43

--- kilo.c
+++ kilo.c
@@ -18,6 +18,7 @@
 /*** data ***/
 
 struct editorConfig {
+  int cx, cy;
   int screenrows;
   int screencols;
   struct termios orig_termios;
@@ -179,6 +180,9 @@ void editorProcessKeypress() {
 /*** init ***/
 
 void initEditor() {
+  E.cx = 0;
+  E.cy = 0;
+
   if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
