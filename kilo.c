5ebc0cf95b8383822848800070ff416f1c940834
mysangle <mysangle@gmail.com>
2017-04-08 18:17:17 +0900

    step 73

--- kilo.c
+++ kilo.c
@@ -228,6 +228,12 @@ void editorScroll() {
   if (E.cy >= E.rowoff + E.screenrows) {
     E.rowoff = E.cy - E.screenrows + 1;
   }
+  if (E.cx < E.coloff) {
+    E.coloff = E.cx;
+  }
+  if (E.cx >= E.coloff + E.screencols) {
+    E.coloff = E.cx - E.screencols + 1;
+  }
 }
 void editorDrawRows(struct abuf *ab) {
   int y;
