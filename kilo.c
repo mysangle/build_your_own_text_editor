451ace7a35c7b6262959e56009d7cecc9ef5305b
mysangle <mysangle@gmail.com>
2017-04-09 15:04:46 +0900

    step 118

--- kilo.c
+++ kilo.c
@@ -553,7 +553,8 @@ void editorProcessKeypress() {
     case BACKSPACE:
     case CTRL_KEY('h'):
     case DEL_KEY:
-      /* TODO */
+      if (c == DEL_KEY) editorMoveCursor(ARROW_RIGHT);
+      editorDelChar();
       break;
 
     case PAGE_UP:
