636fecaccd40de72a7983f1ffceb335dcb4ed167
mysangle <mysangle@gmail.com>
2017-04-09 13:24:00 +0900

    step 113

--- kilo.c
+++ kilo.c
@@ -230,6 +230,7 @@ void editorAppendRow(char *s, size_t len) {
   editorUpdateRow(&E.row[at]);
 
   E.numrows++;
+  E.dirty++;
 }
 
 void editorRowInsertChar(erow *row, int at, int c) {
@@ -239,6 +240,7 @@ void editorRowInsertChar(erow *row, int at, int c) {
   row->size++;
   row->chars[at] = c;
   editorUpdateRow(row);
+  E.dirty++;
 }
 
 /*** editor operations ***/
