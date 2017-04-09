66e53659bc7907776ebf1b0651596c4d18351004
mysangle <mysangle@gmail.com>
2017-04-09 19:12:41 +0900

    step 143

--- kilo.c
+++ kilo.c
@@ -45,6 +45,7 @@ typedef struct erow {
   int rsize;
   char *chars;
   char *render;
+  unsigned char *hl;
 } erow;
 
 struct editorConfig {
@@ -245,6 +246,7 @@ void editorInsertRow(int at, char *s, size_t len) {
 
   E.row[at].rsize = 0;
   E.row[at].render = NULL;
+  E.row[at].hl = NULL;
   editorUpdateRow(&E.row[at]);
 
   E.numrows++;
