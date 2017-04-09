4ddb06e8d7ed28cf74ab75f3fefbfabed903efab
mysangle <mysangle@gmail.com>
2017-04-09 15:50:46 +0900

    step 129

--- kilo.c
+++ kilo.c
@@ -363,6 +363,10 @@ void editorOpen(char *filename) {
 void editorSave() {
   if (E.filename == NULL) {
     E.filename = editorPrompt("Save as: %s");
+    if (E.filename == NULL) {
+      editorSetStatusMessage("Save aborted");
+      return;
+    }
   }
 
   int len;
